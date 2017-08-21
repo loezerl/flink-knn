70fbd44d89bdbffc8108701d4ce3ce9d4fd7f00d
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-21 20:41:47 -0300

    Procurar solucao para carregar um DataSet<Instance>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#flink-knn
+# flink-knn
 
 ### Notas:
 
@@ -6,3 +6,6 @@
 - Implementar estrutura do Experimenter.
 - Implementar os módulos do KNN.
 - Implementar os módulos do Prequential.
+
+Pelo jeito não adiantou usar a lib para carregar os arquivos arff, tenho que dar um jeito de conseguir um `DataSet<Instance>` para realizar as outras operações.
+
--- src/main/java/Experimenter.java
+++ src/main/java/Experimenter.java
@@ -1,11 +1,25 @@
+import eu.amidst.core.datastream.DataInstance;
+import eu.amidst.flinklink.core.data.DataFlink;
+import eu.amidst.flinklink.core.io.DataFlinkLoader;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.IterativeDataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
+
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import classifiers.KNN;
 
 /**
  * Created by loezerl-fworks on 21/08/17.
@@ -19,19 +33,51 @@ public class Experimenter {
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
 
-        BufferedReader reader =
-                new BufferedReader(new FileReader("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff"));
-        System.out.println("Reader criado");
-        ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
-        System.out.println("Arff Reader Criado");
-        Instances data = arff.getData();
-        if (data.classIndex() == -1) {
-            data.setClassIndex(data.numAttributes() - 1);
-        }
+//        BufferedReader reader =
+//                new BufferedReader(new FileReader("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff"));
+//        System.out.println("Reader criado");
+//        ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
+//        System.out.println("Arff Reader Criado");
+//        Instances data = arff.getData();
+//        if (data.classIndex() == -1) {
+//            data.setClassIndex(data.numAttributes() - 1);
+//        }
+//
+//        List<Instance> Teste = new ArrayList<>();
+//
+//        for(Instance exm: data){
+//            Teste.add(exm);
+//        }
+//
+//        List<String> kkk = new ArrayList<>();
+//        kkk.add("KKK 1");
+//        kkk.add("KKK 2");
+//        kkk.add("KKK 3");
+//
+//        Instance ins1 = Teste.get(2);
+//        Instance ins2 = Teste.get(3);
+//        Instance ins3 = Teste.get(4);
+//
+//        System.out.println(Teste.get(10));
+//        Tuple2<Integer, Instance> vamosla = new Tuple2<>(1, ins1);
+//
+//        TypeInformation<Instance> info = TypeInformation.of(new TypeHint<Instance>(){});
+//
+//        DataSet<Tuple2<Integer, Instance>> sss = env.fromElements(vamosla);
 
-        
+        DataFlink<DataInstance> dataSimple =
+                DataFlinkLoader.open(env, "/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff", false);
 
+        List<DataInstance> aham = dataSimple.getDataSet().collect();
 
-        System.out.println("Teste");
+//        for(DataInstance ex: aham){
+//            System.out.println(ex.);
+//        }
+
+        System.out.println(dataSimple.getAttributes());
+
+//        DataSet<String> kks = env.fromCollection(kkk);
+//        System.out.println(dataSimple.getBatchedDataSet(32).count());
+
     }
 }
