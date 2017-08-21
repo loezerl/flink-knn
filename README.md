6318c5ff6e17023312821979959d410184978d32
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-21 17:10:41 -0300

    Dependencia do flink resolvida. Implementar as coisas anotadas.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #flink-knn
+
+### Notas:
+
+- Flink já está funcionando com a IDE.
+- Implementar estrutura do Experimenter.
+- Implementar os módulos do KNN.
+- Implementar os módulos do Prequential.
--- src/main/java/Experimenter.java
+++ src/main/java/Experimenter.java
@@ -1,5 +1,18 @@
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+
 /**
  * Created by loezerl-fworks on 21/08/17.
  */
+
+
 public class Experimenter {
+
+    public static void main(String args[]) throws Exception{
+        // set up the execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        System.out.println("Teste");
+    }
 }
